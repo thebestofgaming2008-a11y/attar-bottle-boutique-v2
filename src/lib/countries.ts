9c2676e6c5b1815114745b27b0fde6b8d71d285d
BOBO <thebestofgaming2008@gmail.com>
2026-08-29 13:39:23 +0200

    Use canonical ISO country options

--- src/lib/countries.ts
+++ src/lib/countries.ts
@@ -17,30 +17,22 @@ const PREFERRED_CODES = [
   "ZA",
 ] as const;
 
-const NON_SHIPPING_REGION_CODES = new Set([
-  "AC",
-  "AN",
-  "BU",
-  "CP",
-  "CS",
-  "DG",
-  "EA",
-  "EU",
-  "EZ",
-  "FX",
-  "IC",
-  "NT",
-  "QO",
-  "SU",
-  "TA",
-  "TP",
-  "UK",
-  "UN",
-  "YD",
-  "YU",
-  "ZR",
-  "ZZ",
-]);
+const ISO_COUNTRY_CODES = `
+AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ
+BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ
+CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ
+DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR
+GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY
+HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP
+KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY
+MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ
+NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY
+QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ
+TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ
+VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW
+`
+  .trim()
+  .split(/\s+/);
 
 const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
 
@@ -52,10 +44,7 @@ function countryFlag(code: string) {
     .join("");
 }
 
-const generatedCountries = Array.from({ length: 26 * 26 }, (_, index) => {
-  const code = `${String.fromCharCode(65 + Math.floor(index / 26))}${String.fromCharCode(
-    65 + (index % 26),
-  )}`;
+const generatedCountries = ISO_COUNTRY_CODES.map((code) => {
   const name = regionNames.of(code);
   return { code, name };
 }).filter(
@@ -63,8 +52,7 @@ const generatedCountries = Array.from({ length: 26 * 26 }, (_, index) => {
     typeof country.name === "string" &&
     Boolean(country.name) &&
     country.name !== country.code &&
-    !country.name.toLowerCase().includes("unknown region") &&
-    !NON_SHIPPING_REGION_CODES.has(country.code),
+    !country.name.toLowerCase().includes("unknown region"),
 );
 
 const preferredRank = new Map<string, number>(PREFERRED_CODES.map((code, index) => [code, index]));
